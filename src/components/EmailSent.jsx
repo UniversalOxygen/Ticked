eefe2c7ab180050a1882866032a2f6c9b469d101
OLAGOKE TUNDE OLUJIDE <olagoketunde600@gmail.com>
2022-11-18 17:52:17 +0100

    password reset-Email task

---
+++ src/components/EmailSent.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const EmailSent = () => {
+  return (
+    <container>
+        <section>
+            <div id='EmailCon'>
+                <img src="" alt="" />
+                <h1>Check your mail</h1>
+                <p>We have sent a verification code to <br /><em>annadavid@</em></p>
+                <button>Go to Email App</button>
+                <p id='code'>Resend verification code</p>
+            </div>
+        </section>
+    </container>
+  )
+}
+
+export default EmailSent

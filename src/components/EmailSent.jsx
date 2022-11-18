9b9925ff9c85438f66b93290f7b2c14780956a02
OLAGOKE TUNDE OLUJIDE <olagoketunde600@gmail.com>
2022-11-18 22:23:28 +0100

    complete Frame work

--- src/components/EmailSent.jsx
+++ src/components/EmailSent.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
+import Sms from '../components/images/sms.png'
+import Pimg from '../components/images/passwordreset-img.png'
 
 const EmailSent = () => {
   return (
-    <container>
-        <section>
-            <div id='EmailCon'>
-                <img src="" alt="" />
-                <h1>Check your mail</h1>
-                <p>We have sent a verification code to <br /><em>annadavid@</em></p>
-                <button>Go to Email App</button>
-                <p id='code'>Resend verification code</p>
+        <section id='EmailCon'>
+            <div id='Emaildetails'>
+                <div id='econtainer'>
+                    <img id='Sms'  src={Sms} alt="Footer_Logo" />
+                    <h1>Check your mail</h1>
+                    <h6>We have sent a verification code to <br />annadavid@gmail.com</h6>
+                    <button>Go to Email App  &nbsp; &#8594;</button>
+                    <p id='code'>Resend verification code </p>
+                    <p>Did you recieve the email? Check your spam filter <br /> or <em>try another email address</em></p>
+                </div>
+            </div>
+            <div id='displayCon'>
+                <img id='Pimg'  src={Pimg} alt="Footer_Logo" />
             </div>
         </section>
-    </container>
   )
 }
 

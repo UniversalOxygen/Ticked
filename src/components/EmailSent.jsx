5d9bc534364beae0386dc567b63cbc81a5b4a843
OLAGOKE TUNDE OLUJIDE <olagoketunde600@gmail.com>
2022-11-19 11:35:49 +0100

    finalized

--- src/components/EmailSent.jsx
+++ src/components/EmailSent.jsx
@@ -9,15 +9,16 @@ const EmailSent = () => {
                 <div id='econtainer'>
                     <img id='Sms'  src={Sms} alt="Footer_Logo" />
                     <h1>Check your mail</h1>
-                    <p>We have sent a verification code to <br />annadavid@gmail.com</p>
+                    <p id='email'>We have sent a verification code to <br />annadavid@gmail.com</p>
                     <button>Go to Email App  &nbsp; &#8594;</button>
                     <h6 id='code'>Resend verification code </h6>
+                    <h6 id='help'>Contact help center </h6>
                     <p>Did you recieve the email? Check your spam filter <br /> or <em id='address'>try another email address</em></p>
                 </div>
             </div>
-            <div id='displayCon'>
+            {/* <div id='displayCon'>
                 <img id='Pimg'  src={Pimg} alt="Footer_Logo" />
-            </div>
+            </div> */}
         </section>
   )
 }
